241ee7ffd1677778a684522bc28eda76eda88d5b
whiskas009 <76867818+whiskas009@users.noreply.github.com>
2022-05-19 22:01:24 +0300

    #1

---
+++ Task_16_Multiplicity/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_16_Multiplicity
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lowlimitMultiples = 100;
+            int highlimitMultiples = 1000;
+            int numbersMultiples = 0;
+
+            int lowDiapasoneDivider = 1;
+            int highDiapasoneDivider = 28;
+
+            Random random = new Random();
+            int randomDivider = random.Next(lowDiapasoneDivider, highDiapasoneDivider);
+
+            for (int i = randomDivider; i < highlimitMultiples; i += randomDivider)
+            {
+                if (i >= lowlimitMultiples)
+                    numbersMultiples++;
+            }
+
+            Console.WriteLine(numbersMultiples);
+        }
+    }
+}
